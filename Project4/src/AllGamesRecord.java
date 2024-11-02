0108dbd6b481a90b54642d09fdd45eeb0cc5d549
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2024-11-01 18:47:10 -0700

    Wrote add and average

--- Project4/src/AllGamesRecord.java
+++ Project4/src/AllGamesRecord.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllGamesRecord {
+    List<GameRecord> gameRecords = new ArrayList<GameRecord>();
+
+    /**
+     * Adds given gameRecord to list of gameRecords.
+     * @param gameRecord gameRecord to be added
+     */
+    public void add(GameRecord gameRecord) {
+        gameRecords.add(gameRecord);
+    }
+
+    /**
+     * Compute average score (sum/count)
+     * @return Double average of all scores
+     */
+    public Double average() {
+        int totalScore = 0;
+        for(GameRecord gameRecord : gameRecords) {
+            totalScore += gameRecord.getScore();
+        }
+        return (double) totalScore / (double) gameRecords.size();
+    }
 }
